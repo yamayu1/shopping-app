52f2f078b2028336ea54ef52d22d84a65db87acf
yamayu1 <yuuyayamada1@gmail.com>
2025-12-14 11:30:00 +0900

    orders・order_itemsテーブルとActiveStorageの作成

---
+++ user-api/db/migrate/008_create_order_items.rb
@@ -0,0 +1,15 @@
+class CreateOrderItems < ActiveRecord::Migration[7.0]
+  def change
+    create_table :order_items, if_not_exists: true do |t|
+      t.references :order, null: false, foreign_key: true
+      t.references :product, null: false, foreign_key: true
+      t.integer :quantity, null: false
+      t.decimal :price, precision: 10, scale: 2, null: false
+      t.decimal :total, precision: 10, scale: 2
+      t.json :product_snapshot
+      t.timestamps
+    end
+
+    add_index :order_items, [:order_id, :product_id] unless index_exists?(:order_items, [:order_id, :product_id])
+  end
+end
